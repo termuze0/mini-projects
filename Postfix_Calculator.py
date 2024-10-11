6075c2503e7448b779f108c004728d9801bcd761
termuze <wertman99999@gmail.com>
2024-10-11 16:30:10 -0400

    postfix calculator

---
+++ Postfix_Calculator.py
@@ -0,0 +1,39 @@
+def is_operator(op):
+    """Check if the character is a valid operator."""
+    return op in ['+', '-', '*', '/']
+
+
+def evaluate_postfix(expression):
+    """Evaluate the given postfix expression."""
+    stack = []
+    
+    expression = expression.split()
+    
+    for token in expression:
+        if not is_operator(token):
+            
+            stack.append(float(token))
+        else:
+           
+            operand2 = stack.pop()  
+            operand1 = stack.pop() 
+            
+            
+            if token == '+':
+                stack.append(operand1 + operand2)
+            elif token == '-':
+                stack.append(operand1 - operand2)
+            elif token == '*':
+                stack.append(operand1 * operand2)
+            elif token == '/':
+                if operand2 == 0:
+                    return "Error: Division by zero"
+                stack.append(operand1 / operand2)
+    
+    return stack[0]
+
+
+if __name__ == "__main__":
+    postfix_expr = "2 3 * 5 4 * + 9 -"  - 9
+    result = evaluate_postfix(postfix_expr)
+    print(f"Result of '{postfix_expr}' is: {result}")
