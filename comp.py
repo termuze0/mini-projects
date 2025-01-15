f5ce3b451b5988fd73bf4109aef99bcdaa445a29
termuze0 <142981640+termuze0@users.noreply.github.com>
2025-01-15 22:40:11 +0300

    Update comp.py

--- comp.py
+++ comp.py
@@ -2,13 +2,12 @@ def simple_compiler():
     print("Simple Python Compiler")
     print("Type your Python code below. Type 'exit' to quit.")
     while True:
-        try:
-            # Input code from the user
+        
             code = input(">>> ")
             if code.lower() == 'exit':  # Exit condition
                 print("Exiting compiler. Goodbye!")
                 break
-            # Execute the code dynamically
+            
             exec(code)
         except Exception as e:
             # Handle and display any runtime errors
